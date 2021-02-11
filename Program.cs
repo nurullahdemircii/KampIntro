c7c26bc98a0e373787717ce5a3a3155c13fff746
Nurullah Demirci <nurullahdemirci061@gmail.com>
2021-02-11 19:05:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Type safety - Tip güvenliği
+            // Do not repeat yourself - Kendini tekrarlama
+            // Değer tutucu, alias
+
+            // string değişkenlere " ile tanımlama yapılır.
+            string categoryLabel = "Kategori"; // kategori etiketi
+            int studentNum = 32000; // öğrenci sayısı
+            double bankRate = 1.45; // faizoranı
+            bool loginControl = true;
+
+            Console.WriteLine(categoryLabel);
+            Console.WriteLine(studentNum);
+            Console.WriteLine(bankRate);
+            Console.WriteLine(loginControl);
+
+            if (loginControl == true)
+            {
+                Console.WriteLine("Success Login.");
+            }
+            else if (loginControl == false)
+            {
+                Console.WriteLine("Fault Login! Please repeat again.");
+            }
+            else
+            {
+                Console.WriteLine("Fault Login!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
